b36531166a509569223c54836b8c4ae92ba7c1ae
VaNomad <sjur.hassel@gmail.com>
2023-05-28 17:33:31 +0100

    Adds dynamic window width with cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
-
+import { useState, useEffect } from "react";
 
 function App() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  const handleResize = () => {
+    setWindowWidth(window.innerWidth)
+  }
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize)
+
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
 
   return (
     <>
-      <div className="m-0 min-w-[320px] h-screen flex justify-center items-center">
-        <h1>Hi</h1>
+      <div className="m-0 min-w-[320px] h-screen flex flex-col justify-center items-center">
+        <h1 className="text-[30px] mb-5">Window Width</h1>
+        <div className="text-2xl text-fuchsia-400">{windowWidth} px</div>
       </div>
     </>
   );
