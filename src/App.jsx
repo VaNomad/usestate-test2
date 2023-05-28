b1485f04dd813d7cc8e12c86477879c8f1902ea4
VaNomad <sjur.hassel@gmail.com>
2023-05-28 15:37:05 +0100

    Vite + Tailwind setup + file/code stripping

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+
+  return (
+    <>
+      <div className="m-0 min-w-[320px] h-screen flex justify-center items-center">
+        <h1>Hi</h1>
+      </div>
+    </>
+  );
+}
+
+export default App
